90d3d7781815740b1d46c727a7b577c9134186c2
Tanel Lebedev <tanel.lebedev@gmail.com>
2016-04-14 12:14:53 +0300

    pressing arrow key changes direction

--- snake.js
+++ snake.js
@@ -1,3 +1,6 @@
+/*global window, document */
+"use strict";
+
 /*
 Your task is to create good old snake game where snake runs in a box and eats treats.
 
@@ -26,14 +29,60 @@ Game features:
 •         display score
 •         display level
 */
-var snakeGame = {
-        gameState: {
-            level: 1,
-            score: 0,
-            paused: false,
-        },
-        config: {
-            pixelSize: 1,
-            boxSize: 20,
-        },
-    };
+
+var Snake = {};
+
+Snake.Direction = {
+    Up: 38,
+    Down: 40,
+    Left: 37,
+    Right: 39,
+};
+
+Snake.Config = function () {
+    this.pixelSize = 1;
+    this.boxSize = 20;
+};
+
+Snake.State = function () {
+    this.level = 1;
+    this.score = 0;
+    this.paused = false;
+    this.intervalMillis = 1000;
+    this.direction = Snake.Direction.Up;
+};
+
+Snake.Game = function (doc, wnd) {
+    this.config = new Snake.Config();
+    this.state = new Snake.State();
+    doc.onkeydown = this.onkeydown;
+    this.timer = wnd.setInterval(this.loop.bind(this), this.state.intervalMillis);
+};
+
+Snake.Game.prototype.update = function () {
+    console.log('update');
+};
+
+Snake.Game.prototype.draw = function () {
+    console.log('draw');
+};
+
+Snake.Game.prototype.onkeydown = function (evt) {
+    switch (evt.keyCode) {
+    case Snake.Direction.Up:
+    case Snake.Direction.Down:
+    case Snake.Direction.Left:
+    case Snake.Direction.Right:
+        console.log('new direction', evt.keyCode);
+        this.direction = evt.keyCode;
+        break;
+    }
+    return true;
+};
+
+Snake.Game.prototype.loop = function () {
+    this.update();
+    this.draw();
+};
+
+document.game = new Snake.Game(document, window);
